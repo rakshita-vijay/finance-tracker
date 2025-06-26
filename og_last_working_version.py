61ff6209dd2f14e95bf28068222a73e8ff649488
Rakshita Vijay <rakshita.vijay1@gmail.com>
2025-06-26 18:57:18 +0530

    last working version added into the og file

--- og_last_working_version.py
+++ og_last_working_version.py
@@ -13,42 +13,121 @@ def get_budgets_list():
   frs = fr.split(', ')
   return frs
 
-def main():
-  frs = get_budgets_list()
-
-  mb = re.search(r'monthly = (\d+)', frs[0].strip()).group(1)
-  yb = re.search(r'yearly = (\d+)', frs[1].strip()).group(1)
-
-  ch = 'd'
-  while ch.lower()[0] != 'y' and ch.lower()[0] != 'n':
-    ch = input(f"Do you wish to keep your pre-saved monthly and yearly budgets ({mb} and {yb})? Enter 'y' for yes and 'n' for no: ")
-
-  choice = ch.lower()[0]
-
-  if choice == 'n':
-    changeBudget()
-  else:
-    bl = get_budgets_list()
-    displayBudget(bl)
-
-  frs = get_budgets_list()
-
-  budgets = {
-    'monthly': frs[0].strip('monthly = '),
-    'yearly': frs[1].strip('yearly = ')
-  }
-
+def find_csv_file_location():
   curr_csv = ""
-  curr_pdf = ""
 
   for folders, _, files in os.walk("./saved_files"):
     for file in files:
       if file[0:3] == 'csv':
         curr_csv = ''.join(os.path.join(os.getcwd(), os.path.join(folders, file)).split('./'))
-      elif file[0:3] == 'pdf':
-        curr_pdf = ''.join(os.path.join(os.getcwd(), os.path.join(folders, file)).split('./'))
+        break
 
-  todayyy = datetime.datetime.today()
+  return curr_csv
+
+def choice_to_change_or_keep_current_budget():
+  bud_list = get_budgets_list()
+
+  # mb = re.search(r'monthly = (\d+)', bud_list[0].strip()).group(1)
+  # yb = re.search(r'yearly = (\d+)', bud_list[1].strip()).group(1)
+
+  ch = 'd'
+  while ch.lower()[0] != 'y' and ch.lower()[0] != 'n':
+    print("Current budgets:")
+    displayBudget(bud_list)
+    ch = input(f"\nDo you want to change them? Enter 'y' for yes and 'n' for no: ")
+
+  choice = ch.lower()[0]
+
+  if choice == 'y':
+    changeBudget()
+    print("\n{}".format("~" * 120))
+  else:
+    print("\n{}".format("~" * 120))
+  # else:
+  #   bl = get_budgets_list()
+  #   displayBudget(bl)
+
+def main():
+  print("\nWhat are you here to do?")
+  print("1. Add Transaction")
+  print("2. View Spending")
+  print("3. Generate Report")
+  print("4. Exit \n")
+
+  repeat = 'yes'
+  purpose_of_visit = 0
+  while repeat == 'yes' or purpose_of_visit < 1 or purpose_of_visit > 4:
+    try:
+      purpose_of_visit = int(input(f"Enter a valid choice (1-4): "))
+      repeat = 'no'
+    except ValueError as ve:
+      repeat = 'yes'
+      print("\nInvalid input. Please enter a valid integer choice.")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if purpose_of_visit == 1:
+    pass
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  elif purpose_of_visit == 2:
+    print("\n{}\n".format("~" * 120))
+
+    bud_list = get_budgets_list()
+    displayBudget(bud_list)
+    print()
+
+    curr_csv = find_csv_file_location()
+
+    print("Transactions to date: ")
+    display_csv_content(curr_csv)
+    print("\n{}\n".format("~" * 120))
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  elif purpose_of_visit == 3:
+    pass
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  elif purpose_of_visit == 4:
+    print("Exiting...")
+    sys.exit(1)
+
+  choice_to_change_or_keep_current_budget()
+
+  continue_or_not = 'd'
+  while continue_or_not.lower()[0] != 'y' and continue_or_not.lower()[0] != 'n':
+    continue_or_not = input("\nWould you like to make more changes? Enter 'y' for yes and 'n' for no: ")
+
+  c_or_e = continue_or_not.lower()[0]
+
+  if c_or_e == 'y':
+    print("\n{}".format("~" * 120))
+    main()
+  else:
+    print("Exiting...")
+    sys.exit(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+  bud_list = get_budgets_list()
+
+  budgets = {
+    'monthly': bud_list[0].strip('monthly = '),
+    'yearly': bud_list[1].strip('yearly = ')
+  }
 
   # csv_fp = open(curr_csv, 'w', encoding='utf-8')
   # csv_writer = csv.writer(csv_fp)
@@ -57,9 +136,12 @@ def main():
   # csv_fp.close()
 
   # transactions
-  print("\nCurrent transactions: ")
-  display_csv_content(curr_csv)
 
+
+  todayyy = datetime.datetime.today()
+
+
+'''
 
 
 

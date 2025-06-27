320ede3b0d5728be960f891c6d166163c127d0b2
Rakshita Vijay <rakshita.vijay1@gmail.com>
2025-06-27 09:47:50 +0530

    haven't tested yet, just saving code
    has been done:
    changed downloading from need_to_go_through to optional
    part of add_trans section
    added "get the transaction to add in the csv file" function into csv_methods.py
    
    to do:
    finish add_transaction section: change time stamp of the csv file in the saved_files directory
    Generate Report - entire section, with crewai
    decide whether change_budget should have its own function
    add choice to download report, if it exists, in " if purpose_of_visit == 5 " section
    change download_file() function to allow from download of .md/.txt file (write to it, same as rag writing)
    under budget or over budget
    show budget left

--- file_methods/csv_file_methods.py
+++ file_methods/csv_file_methods.py
@@ -1,4 +1,4 @@
-import os, csv, sys
+import os, csv, sys, datetime
 from crewai_toolkits_gem_2point0_flash.transform_csv_to_md_table import transformed_table
 
 def extract_csv_content(curr_csv):
@@ -24,6 +24,112 @@ def find_csv_file_location():
         break
 
   return curr_csv
+
+def get_trans_line_details():
+  csv_file_loc = find_csv_file_location() # gives full path
+  csv_file_lines = extract_csv_content() # gives list of lists
+
+  tran_done = csv_file_lines - 1 # because heading also exists
+  curr_tran = tran_done + 1 # current transaction
+
+  fields = ['S.NO', 'DATE', 'DESCRIPTION', 'AMOUNT', 'NOTES']
+  responses = []
+  responses.append(curr_tran)
+
+  # formet: ['int', 'MM/DD/YYYY', 'text (<70 characters)', 'float', 'text (<70 characters)']
+
+  # DATE
+  thro_err = True
+  while thro_err == True:
+    try:
+      resp = input("Enter the date in MM/DD/YYYY format: ")
+      stripped_resp = resp.strip()
+
+      if len(stripped_resp) > len('MM/DD/YYYY'):
+        raise Exception
+
+      # frmt_to_srch = r'(\d{2})(\D)(\d{2})(\D)(\d{2}\d{2}?)'
+
+      if len(stripped_resp) == len('MM/DD/YYYY'):
+        frmt_to_srch = r'(\d{2})(\D)(\d{2})(\D)(\d{4})'
+      elif len(stripped_resp) == len('MM/DD/YY'):
+        frmt_to_srch = r'(\d{2})(\D)(\d{2})(\D)(\d{2})'
+      else:
+        raise Exception
+
+      if re.search(frmt_to_srch, stripped_resp) == None:
+        raise Exception
+      else:
+        thro_err = False
+
+    except Exception as ex:
+      # thro_err = True
+      print("Invalid date format. Retry.")
+
+  responses.append(stripped_resp)
+
+  # DESCRIPTION
+  thro_err = True
+  while thro_err == True:
+    try:
+      resp = input("Enter the description as text (<70 characters): ")
+      stripped_resp = resp.strip()
+
+      if len(stripped_resp) > 70:
+        raise Exception
+      else:
+        thro_err = False
+
+    except Exception as ex:
+      # thro_err = True
+      print("Message is too long. Retry.")
+
+  responses.append(stripped_resp)
+
+  # AMOUNT
+  thro_err = True
+  while thro_err == True:
+    try:
+      resp = float(input("Enter the amount as a float number (xx.yyy): "))
+      thro_err = False
+
+    except Exception as ex:
+      # thro_err = True
+      print("Invalid amount format. Retry.")
+
+  responses.append(resp)
+
+  # NOTES
+  thro_err = True
+  while thro_err == True:
+    try:
+      resp = input("Enter notes (if there are any) as text (<70 characters): ")
+      stripped_resp = resp.strip()
+
+      if len(stripped_resp) > 70:
+        raise Exception
+      else:
+        thro_err = False
+
+    except Exception as ex:
+      # thro_err = True
+      print("Message is too long. Retry.")
+
+  responses.append(stripped_resp)
+
+  return responses
+
+def add_to_csv(list_or_listOfLists, list_len):
+  csv_file = open(curr_csv, mode='a', encoding='utf-8')
+  csv_wrtr = csv.writer(csv_file)
+
+  if list_len == 1:
+    csv_wrtr.writerow(list_or_listOfLists[0])
+  else:
+    csv_wrtr.writerows(list_or_listOfLists)
+
+  csv_file.close()
+
 
 # def add_lines_to_csv(<list of variables or cell values>):
 #   pass
--- main_interface.py
+++ main_interface.py
@@ -6,7 +6,7 @@ from core.budget import changeBudget, displayBudget
 
 from download_to_device import download_file
 
-from file_methods.csv_file_methods import display_csv_content, find_csv_file_location
+from file_methods.csv_file_methods import display_csv_content, find_csv_file_location, get_trans_line_details
 
 l_only_line_demarcator = "\n{}".format("~" * 120)
 r_only_line_demarcator = "{}\n".format("~" * 120)
@@ -39,16 +39,18 @@ def choice_to_change_or_keep_current_budget():
 
 def main():
   print("\nWhat are you here to do?")
-  print("1. Add Transaction")
+  print("1. Add Transaction(s)")
   print("2. View Spending")
   print("3. Generate Report")
-  print("4. Exit \n")
+  print("4. Change Budget")
+  print("5. Download Report / CSV file / other files")
+  print("6. Exit \n")
 
   repeat = 'yes'
   purpose_of_visit = 0
-  while repeat == 'yes' or purpose_of_visit < 1 or purpose_of_visit > 4:
+  while repeat == 'yes' or purpose_of_visit < 1 or purpose_of_visit > 6:
     try:
-      purpose_of_visit = int(input(f"Enter a valid choice (1-4): "))
+      purpose_of_visit = int(input(f"Enter a valid choice (1-6): "))
       repeat = 'no'
     except ValueError as ve:
       repeat = 'yes'
@@ -58,8 +60,32 @@ def main():
 
   # Add Transaction
   if purpose_of_visit == 1:
-    todayyy = datetime.datetime.today()
-    pass
+
+    thro_err = True
+    while thro_err == True:
+      try:
+        num_of_trans = int(input("Enter the number of transactions you wish to add: "))
+        thro_err = False
+
+      except Exception as ex:
+        # thro_err = True
+        print("Invalid numbers of transactions. Retry.")
+
+    trans_deets = []
+    for trans_num in range(num_of_trans):
+      print(f"Adding transaction #{trans_num+1}: ")
+      trans_deets.append(get_trans_line_details())
+
+    # ensures it is a list of lists
+
+    if num_of_trans == 0:
+      pass
+    else:
+      todayyy = datetime.datetime.today()
+      add_to_csv(trans_deets, len(trans_deets))
+
+
+    # not finished yet
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -75,58 +101,68 @@ def main():
 
     print("Transactions to date: ")
     display_csv_content(curr_csv)
+
     print(l_and_r_line_demarcator)
+    purpose_of_visit = 4
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   # Generate Report
   elif purpose_of_visit == 3:
     pass
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  # Exit
-  elif purpose_of_visit == 4:
-    print("Exiting...")
-    sys.exit(1)
+    # have a choice if they have accidentally pressed 3
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  choice_to_change_or_keep_current_budget()
+  # Change budget
+  if purpose_of_visit == 4:
+    choice_to_change_or_keep_current_budget()
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  dl_or_not = 'd'
-  print("\nWould you like to download one of the two listed below?")
-  print("1. the csv file of your transactions")
-  print("2. any other file in this directory")
+  # Download Report / CSV file / other files
+  elif purpose_of_visit == 5:
 
-  while dl_or_not.lower()[0] not in ['y', 'n', '1', '2']:
-    dl_or_not = input("\nEnter '1'/'2'/'y' for specific/yes and 'n' for no: ")
+    # add the choice to also download report
+    dl_or_not = 'd'
+    print("\nWould you like to download one of the two listed below?")
+    print("1. the csv file of your transactions")
+    print("2. any other file in this directory")
 
-  if dl_or_not.lower()[0] == 'y':
-    num_ch = '0'
-    while num_ch.lower()[0] not in ['1', '2']:
-      num_ch = input("\nEnter '1' for csv and '2' for other: ")
+    while dl_or_not.lower()[0] not in ['y', 'n', '1', '2']:
+      dl_or_not = input("\nEnter '1'/'2'/'y' for specific/yes and 'n' for no: ")
 
-    if num_ch.lower()[0] == '1':
+    if dl_or_not.lower()[0] == 'y':
+      num_ch = '0'
+      while num_ch.lower()[0] not in ['1', '2']:
+        num_ch = input("\nEnter '1' for csv and '2' for other: ")
+
+      if num_ch.lower()[0] == '1':
+        print(l_and_r_line_demarcator)
+        download_file()
+      else:
+        dl_fp = input("\nEnter file path or name in this directory: ")
+        print(l_only_line_demarcator)
+        download_file(dl_fp)
+
+    elif dl_or_not.lower()[0] == '1':
       print(l_and_r_line_demarcator)
       download_file()
-    else:
+
+    elif dl_or_not.lower()[0] == '2':
       dl_fp = input("\nEnter file path or name in this directory: ")
       print(l_only_line_demarcator)
       download_file(dl_fp)
 
-  elif dl_or_not.lower()[0] == '1':
-    print(l_and_r_line_demarcator)
-    download_file()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  elif dl_or_not.lower()[0] == '2':
-    dl_fp = input("\nEnter file path or name in this directory: ")
-    print(l_only_line_demarcator)
-    download_file(dl_fp)
+  # Exit
+  elif purpose_of_visit == 6:
+    print("Exiting...")
+    sys.exit(1)
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   continue_or_not = 'd'
@@ -181,8 +217,6 @@ if __name__ == "__main__":
 
 
 '''
-to also accept float values
 under budget or over budget
 show budget left
-auto download of csv onto system
 '''
